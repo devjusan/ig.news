f2ab4d04d0f935a8c4bdf6244e54efb6358a7459
Junior Santos <valmir.santos@zukk.com.br>
2022-05-02 20:07:12 -0300

    feat: button

---
+++ src/components/subscribe-button/index.tsx
@@ -0,0 +1,8 @@
+import styles from "./styles.module.scss";
+
+export const SubscribeButton = () => (
+  <button type="button" className={styles.subscribeButton}>
+    {" "}
+    subscribe
+  </button>
+);
