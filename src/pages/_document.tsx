35983b00c73b22fb3f1886068c8a8f965eef6f57
Junior Santos <valmir.santos@zukk.com.br>
2022-05-02 16:27:36 -0300

    refactor: link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
             rel="stylesheet"
           />
+          <link href="../public/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main /> <NextScript />
