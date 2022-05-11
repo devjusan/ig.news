2f3af13afe434524c5fc14abac92f3bde5367a27
Junior Santos <valmir.santos@zukk.com.br>
2022-05-11 16:02:18 -0300

    feat: faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from "faunadb";
 import GithubProvider from "next-auth/providers/github";
 import NextAuth from "next-auth";
+import fauna from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -8,4 +10,18 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    signIn: async (payload) => {
+      const { email } = payload.user;
+
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,7 +1,10 @@
-import Stripe from 'stripe'
-import {name, version} from '../../package.json'
+import Stripe from "stripe";
+import { name, version } from "../../package.json";
 
-export const stripe = new Stripe(process.env.STRIPE_API_KEY, {apiVersion: '2020-08-27', appInfo: {
+export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+  apiVersion: "2020-08-27",
+  appInfo: {
     name,
-    version
-}})
+    version,
+  },
+});
