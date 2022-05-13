ed463b10d14bef5575b577acc46213d773d7599f
Valmir Santos <jnrplay@hotmail.com>
2022-05-13 13:34:13 -0300

    refactor: toast

--- src/components/subscribe-button/index.tsx
+++ src/components/subscribe-button/index.tsx
@@ -1,47 +1,46 @@
 import { signIn, useSession } from "next-auth/react";
 import { useCallback, useState } from "react";
 import styles from "./styles.module.scss";
-import axios from '../../services/axios'
+import axios from "../../services/axios";
 import getStripeJs from "../../services/browser-stripe";
 import { Toast } from "../toast";
-
 
 interface ISubscribeButtonProps {
   priceId: string;
 }
 
-
-
-export const SubscribeButton = ({ priceId }: ISubscribeButtonProps) =>  {
-  const [error, setError] = useState(false)
+export const SubscribeButton = ({ priceId }: ISubscribeButtonProps) => {
+  const [error, setError] = useState(false);
   const { data: session, status } = useSession();
 
   const handleSubscribe = useCallback(async () => {
     if (!session) {
-      signIn('github')
-      return
+      signIn("github");
+      return;
     }
 
     try {
-      const response = await axios.post('/checkout')
+      const response = await axios.post("/checkout");
 
-      const {sessionId} = response.data
+      const { sessionId } = response.data;
 
-      const stripe = await getStripeJs()
-      
-      await stripe.redirectToCheckout(sessionId)
+      const stripe = await getStripeJs();
 
-
-
+      await stripe.redirectToCheckout(sessionId);
     } catch (error) {
-      setError(true)
+      setError(true);
     }
+  }, [session]);
 
-  }, [session])
-
-
-  return error ? <Toast message='Ocorreu um erro na requisição' error />: <button type="button" className={styles.subscribeButton} onClick={handleSubscribe} >
-  {" "}
-  subscribe now
-</button>
-}
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      {" "}
+      subscribe now
+      {error && <Toast message="Ocorreu um erro na requisição" error />}
+    </button>
+  );
+};
--- src/components/toast/index.tsx
+++ src/components/toast/index.tsx
@@ -1,23 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { unmountComponentAtNode } from "react-dom";
+
 import styles from "./styles.module.scss";
 
 interface IToast {
-    message: string
-    error: boolean
+  message: string;
+  error: boolean;
 }
 
-export function Toast({message, error}: IToast) {
-    const [timer, setTimer] = useState(0)
+export function Toast({ message, error }: IToast) {
+  const [counter, setCounter] = useState(0);
 
-    setTimeout(() => {
-        setTimer((state) => state = 1)
+  useEffect(() => {
+    const interval = setTimeout(() => {
+      setCounter(counter + 1);
     }, 1000);
 
-    if (timer === 5) {
-        clearTimeout()
-    }
+    return () => clearTimeout(interval);
+  }, [counter]);
 
-  return (
-    timer < 6 ? error ? <aside id={styles.container} className={styles.error}><p>{message}</p></aside> : <aside id={styles.container} className={styles.success}><p>{message}</p></aside> : null
+  return counter < 6 ? (
+    error ? (
+      <aside id={styles.container} className={styles.error}>
+        <p>{message}</p>
+      </aside>
+    ) : (
+      <aside id={styles.container} className={styles.success}>
+        <p>{message}</p>
+      </aside>
+    )
+  ) : (
+    <></>
   );
 }
