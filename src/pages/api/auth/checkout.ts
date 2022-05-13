a0701790c429085f4fe99aee90cac891ec6f5959
Valmir Santos <jnrplay@hotmail.com>
2022-05-12 21:37:15 -0300

    feat: checkout route

---
+++ src/pages/api/auth/checkout.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { stripe } from "../../../services/stripe";
+
+const checkout = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const { user } = await getSession({ req });
+    const { email } = user;
+
+    const stripeCustomer = await stripe.customers.create({ email });
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
+      payment_method_types: ["card"],
+      billing_address_collection: "required",
+      line_items: [
+        {
+          price: "price_1Kv8RwLGr7l9YRYlgYnQYdX9",
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_URL_ON_PURCHASE,
+      cancel_url: process.env.STRIPE_URL_ON_CANCEL,
+    });
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method now allowed");
+  }
+};
+
+export default checkout;
